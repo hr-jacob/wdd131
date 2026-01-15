6fc6e4e73c38d6273271fb6ee85fcd0978132f68
hr-jacob <jherreraroca@byupathway.edu>
2026-01-15 12:18:45 -0600

    js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,3 +1,22 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('____');
+
+// Create a li element
+const li = document.createElement('li');
+
+// Create a delete button
+const deleteButton = document.createElement('button');
+
+// Populate the li with the input value
+li.textContent = input.value;
+
+// Populate the button with ❌
+deleteButton.textContent = '❌';
+
+// Append the delete button to the li
+li.append(deleteButton);
+
+// Append the li to the unordered list
+list.append(li);
+
