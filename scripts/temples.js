562d2128dc8e1623c4e20ea953f46450e9fa5a59
hr-jacob <jherreraroca@byupathway.edu>
2026-01-16 09:29:40 -0600

    date

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,8 @@
 
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get the last modified date
+document.getElementById("lastModified").innerHTML =
+  "Last Modification: " + document.lastModified;
