1900cbc9b906dde8c40bcc97d28ad932d1174548
hr-jacob <jherreraroca@byupathway.edu>
2026-01-17 12:36:52 -0600

    css corrected

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,9 @@
-// ===== FOOTER DATES =====
 const yearSpan = document.querySelector('#currentyear');
 const lastModified = document.querySelector('#lastModified');
 
 yearSpan.textContent = new Date().getFullYear();
 lastModified.textContent = `Last Modification: ${document.lastModified}`;
 
-// ===== HAMBURGER MENU =====
 const menuButton = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation ul');
 
