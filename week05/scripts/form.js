5fa0db7cf7105d976ff4ade659c7b3da8e77001a
hr-jacob <jherreraroca@byupathway.edu>
2026-02-05 14:28:02 -0600

    js

--- week05/scripts/form.js
+++ week05/scripts/form.js
@@ -1,3 +1,11 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get the last modified date
+document.getElementById("lastModified").innerHTML =
+  "Last Modification: " + document.lastModified;
+
 // Product Array (From Instructions)
 const products = [
   {
