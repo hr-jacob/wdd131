92a6d6f4462a5c3c589bd8e77bebd080c6d6cd36
hr-jacob <jherreraroca@byupathway.edu>
2026-02-09 16:15:42 -0600

    js

---
+++ project/scripts/bakery.js
@@ -0,0 +1,26 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+const temperature = 10; 
+const windSpeed = 5;  
+
+const windChillElement = document.querySelector("#windchill");
+
+function calculateWindChill(temp, speed) {
+  return (
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(speed, 0.16) +
+    0.3965 * temp * Math.pow(speed, 0.16)
+  ).toFixed(1);
+}
+
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChillElement.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+  windChillElement.textContent = "N/A";
+}
+
